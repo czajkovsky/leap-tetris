cc6571d7be9973a6f228453dae82b4abd4536c4e
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-01-12 13:17:58 +0100

    finish rewriting

--- TetrisLeap.py
+++ TetrisLeap.py
@@ -23,6 +23,45 @@ class GameEngine:
     self.board.toggle_grid()
     self.board.update(pygame.time.get_ticks())
 
+  def start_move(self, direction):
+    if direction == 'DOWN':
+      self.board.key_down_flag = True
+    else:
+      if self.board.last_strafe == 0:
+        if direction == 'LEFT':
+          self.board.current_gp.move_left()
+          self.board.key_left_flag = True
+        elif direction == 'RIGHT':
+          self.board.current_gp.move_right()
+          self.board.key_right_flag = True
+        self.board.last_strafe = (pygame.time.get_ticks()+250)
+
+  def stop_move(self, direction):
+    if direction == 'DOWN':
+      self.board.key_down_flag = False
+    else:
+      self.board.last_strafe = 0
+      if direction == 'LEFT':
+        self.board.key_left_flag = False
+      elif direction == 'RIGHT':
+        self.board.key_right_flag = False
+
+  def rotate(self):
+    self.board.current_gp.rotate()
+
+  def redraw(self):
+    self.board.update(pygame.time.get_ticks())
+    screen.fill((175,175,175))
+    self.board.draw(screen)
+    if self.board.game_over:
+      self.board.puase = True
+      x = screen_w
+      y = screen_h
+      xx = game_over_image.get_width()
+      yy = game_over_image.get_height()
+      screen.blit(game_over_image, ((x - xx)/2, (y - yy)/2))
+    pygame.display.flip()
+
 def main():
 
   game = GameEngine()
@@ -38,6 +77,22 @@ def main():
           game.stop()
         if event.key == pygame.K_g:
           game.showGrid()
+        if event.key == pygame.K_LEFT:
+          game.start_move('LEFT')
+        if event.key == pygame.K_RIGHT:
+          game.start_move('RIGHT')
+        if event.key == pygame.K_DOWN:
+          game.start_move('DOWN')
+        if event.key == pygame.K_UP:
+          game.rotate()
+      if event.type == pygame.KEYUP:
+        if event.key == pygame.K_DOWN:
+          game.stop_move('DOWN')
+        if event.key == pygame.K_LEFT:
+          game.stop_move('LEFT')
+        if event.key == pygame.K_RIGHT:
+          game.stop_move('RIGHT')
+    game.redraw()
   pygame.quit()
 
 if __name__ == "__main__":
